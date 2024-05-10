3a31d34558dfc1df042e20f6a1e58dcfdde24377
paniz <panizsojaghi@gmail.com>
2024-05-10 16:27:06 -0400

    test

---
+++ src/test/java/io/anserini/collection/SafeTesnorCollectionTest.java
@@ -0,0 +1,11 @@
+package io.anserini.collection;
+
+import org.junit.Test;
+
+public class SafeTesnorCollectionTest {
+    @Test
+    public void testNext() {
+        // SafeTensorCollection collection = new SafeTensorCollection();
+        // collection.next();
+    }
+}
